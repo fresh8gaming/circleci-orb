d0e542d24f4e14384338ed57362f7b81b4b127d2
Chris Augier <chris.augier@gmail.com>
2023-12-04 13:57:02 +0000

    Update cypress project id

--- examples/angular-app/cypress.config.ts
+++ examples/angular-app/cypress.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  projectId: 'mx9dpd',
+  projectId: 'kzhfun',
   e2e: {
     baseUrl: 'http://localhost:4200',
     setupNodeEvents(on, config) {
